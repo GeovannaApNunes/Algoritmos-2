8eb71883a0f657b01418f7e123e62b3f74c0cda7
Geovanna Nunes <geovannanunes349@gmail.com>
2026-08-06 21:37:40 -0300

    Create netos.py

---
+++ netos.py
@@ -0,0 +1,39 @@
+netos = int(input('Sr.Arnaldo quantos netos o senhor tem: '))
+
+if netos <= 3:
+    print('Compre um carro convencional')
+    
+if netos >= 4 and netos <= 7:
+    print('Compre um SUV')
+    
+if netos > 7:
+    print('Compre uma Van')
+
+
+//////////////////////////////////////////////////
+netos = int(input('Sr.Arnaldo quantos netos o senhor tem: '))
+
+if netos <= 3:
+    print('Compre um carro convencional')
+    
+elif netos >= 4 and netos <= 7:
+    print('Compre um SUV')
+    
+elif netos > 7:
+    print('Compre uma Van')
+    
+    
+    
+////////////////////////////////////////////////////
+netos = int(input('Sr.Arnaldo quantos netos o senhor tem: '))
+
+if netos <= 3:
+    print('Compre um carro convencional')
+    
+elif netos >= 4 and netos <= 7:
+    print('Compre um SUV')
+    
+else:
+    print('Compre uma Van')
+    
+    
