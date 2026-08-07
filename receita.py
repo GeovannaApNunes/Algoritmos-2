0ae8bf03cef6bb2c442b2532dfc6a11c16c47b4d
Geovanna Nunes <geovannanunes349@gmail.com>
2026-08-06 21:38:36 -0300

    Add income check and appliance suggestion logic

---
+++ receita.py
@@ -0,0 +1,8 @@
+receita = float(input('Sra. Lourdes quanto a senhora recebe  R$: '))
+
+if receita <= 1500:
+    print('Compre uma geladeira nova')
+    
+else:
+    print('Troque a geladeira e o micro-ondas')
+    
