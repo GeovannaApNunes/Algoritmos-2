f05632d613bf971b03c590dd1608c9e1406813af
Geovanna Nunes <geovannanunes349@gmail.com>
2026-08-06 21:47:24 -0300

    Add civil status validation in estadocivil.py

---
+++ estadocivil.py
@@ -0,0 +1,16 @@
+estado = input('Digite seu estado civil (C , S , D, V): ')
+
+if estado == 'C' or estado == 'c':
+    print('Valido')
+
+elif estado == 'S' or estado == 's':
+    print('Valido')
+
+elif estado == 'D' or estado == 'd':
+    print('Valido')
+
+elif estado == 'V' or estado == 'v':
+    print('Valido')
+    
+else:
+    print('INVALIDO')
