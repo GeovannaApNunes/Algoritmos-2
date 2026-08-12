2a2d9de0ff2723d45ca77e1beee0affabeae0e48
Geovanna Nunes <geovannanunes349@gmail.com>
2026-08-11 21:35:59 -0300

    Add player age input and average calculation

---
+++ idadejogador.py
@@ -0,0 +1,12 @@
+total = 0
+totalreal = 0
+i=1
+
+while i <= 7:
+    idade = int(input('Digite a idade do jogador: '))
+    total = idade + total
+    i=i+1
+    
+totalreal = total /7
+
+print(totalreal)
