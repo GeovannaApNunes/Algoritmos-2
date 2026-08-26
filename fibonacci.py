b2bc7059d509068637da807a61865e33b8419c93
Geovanna Nunes <geovannanunes349@gmail.com>
2026-08-25 21:28:20 -0300

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    a=0
+    b=1
+    for i in range(n):
+        c=a+b
+        a=b
+        b=c
+    return c
+
+pos = int(input('Digite o n-essinho: '))
+print(fibonacci(pos))
+
+
