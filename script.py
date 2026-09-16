de1a357ba195c1adf3645edc5c87cd660c1d0f64
Geovanna Nunes <geovannanunes349@gmail.com>
2026-09-15 21:18:07 -0300

    Add files via upload

--- script.py
+++ script.py
@@ -1,40 +1,17 @@
-'''
-aluno = open('nomes.txt' , 'a')
+def leitura():
+    heroi = open('herois.txt', 'a')
+    nome = input('Digite o nome do heroi: ')
 
-aluno.write('Aparecida' + '\n')
-print('Nome Gravado com sucesso!')
-aluno.close()
-'''
+    heroi.write(nome + '\n')
+    heroi.close()
 
-pergunta = input('Deseja recriar o arquivo: ')
-if pergunta.upper() == 'S':
-    aluno = open('nomes.txt' , 'w')
-    endereco = open('endereco.txt' , 'w')
-else:
-    aluno = open('nomes.txt' , 'a')
-    endereco = open('endereco.txt' , 'a')
-pergunta  = 'S'
-while pergunta == 'S':
-    nome = input('Digite o nome do aluno: ')
-    aluno.write(nome + '\n')
-    endereconome = input('Digite o endereco do aluno: ')
-    endereco.write(endereconome + '\n')
-    pergunta = input('Deseja continuar? [S/N] : ')
-aluno.close()
+def formatar():
+    heroi = open('herois.txt', 'r')
+    report = open('report.txt', 'w')
+    for h in heroi:
+        report.write(h.rstrip() + 'esta presente no filme. \n')
+    heroi.close()
+    report.close()
 
-aluno = open('nomes.txt' , 'r')
-endereco= open('endereco.txt' , 'r')
-
-maluno = aluno.readlines()
-mendereco = endereco.readlines()
-
-i=0
-
-for i in maluno:
-    print( i + 'reside' + mendereco[i])
-    i=i+1
-
-
-print(aluno.read() + endereco.read())
-aluno.close()
-endereco.close()
+leitura()
+formatar()
