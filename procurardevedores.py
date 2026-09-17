4a7dafabe5283082b2f73a8d8fd209bc5070fa7d
Geovanna Nunes <geovannanunes349@gmail.com>
2026-09-16 21:57:17 -0300

    Add files via upload

---
+++ procurardevedores.py
@@ -0,0 +1,38 @@
+import re
+
+def lerDados():
+    nome = open('clientes.txt', 'a')
+    endereco = open('endereco.txt', 'a')
+    titulo = open('duplicata.txt', 'a')
+
+    for i in range(3):
+        n = input('Digite o nome do cliente: ')
+        e = input('Digite o endereco do cliente: ')
+        t = input('Digite o valor do titulo a receber do cliente: ')
+        nome.write(n + '\n')
+        endereco.write(e + '\n')
+        titulo.write(t + '\n')
+
+    nome.close()
+    endereco.close()
+    titulo.close()
+
+def gerarCarta():
+    procurado = input('Digite o termo procurado: ')
+
+    nome = open('clientes.txt', 'r')
+    endereco = open('endereco.txt', 'r')
+    titulo = open('duplicata.txt', 'r')
+
+    carta = open('carta.txt', 'w')
+    for n in  nome:
+        if re.search(procurado, n, re.IGNORECASE):
+                     carta.write( n.upper() + '' + endereco.readline().upper() +
+                     '\n\n' + "Prezado client. \n"+
+                     "Consta um titulo em aberto no valor de R$"+ titulo.readline() + '\n'
+                     + 'Solicitamos a sua visita a nossa loja, a fim de negociarmos sua divida \n'+
+                     'Evite seu registro no SPC\n Atenciosamente, Depto cobranca\n\n\n'
+                     )
+
+lerDados()
+gerarCarta()
